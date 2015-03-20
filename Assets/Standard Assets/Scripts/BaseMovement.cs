326817e9c1c813c27bf606742d66bb492ac7352d
Ching Chow <chingchow7@gmail.com>
2015-03-20 03:52:22 -0500

    More changes to look/feel
    start of basic movement and game scene

---
+++ Assets/Standard Assets/Scripts/BaseMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseMovement : MonoBehaviour {
+
+	public float speed = .15f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		transform.position = new Vector3 ( transform.position.x, transform.position.y, transform.position.z - speed); 
+	}
+}
---
+++ Assets/Standard Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// speed modifiers
+	public float x_speed_mod = .5f;
+	public float y_speed_mod = .5f;
+
+	// input variables
+	private float x_input;
+	private float y_input;
+
+	// speed of movement
+	private float x_speed;
+	private float y_speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//get input
+		x_input = Input.GetAxis ("Horizontal");
+		y_input = Input.GetAxis ("Vertical");
+
+		//output input for debug
+		//Debug.Log ( x_input + " and " + y_input);
+	}
+
+	// Handle physics based on input
+	void FixedUpdate () {
+		x_speed = -1 * x_input * x_speed_mod;
+		y_speed = y_input * y_speed_mod;
+
+		transform.Translate( new Vector3( x_speed, 0, y_speed));
+	}
+}
